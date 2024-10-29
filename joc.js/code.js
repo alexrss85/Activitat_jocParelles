eca48e417dc19e6c469d5457823434cf44900d27
alexrss85 <2023_alex.rosendo@iticbcn.cat>
2024-10-29 01:43:23 +0100

    +

--- joc.js/code.js
+++ joc.js/code.js
@@ -5,18 +5,14 @@ const letras = ["A", "A", "B", "B", "C", "C", "D", "D", "E", "E", "F", "F", "G",
 let parell = [];
 let lletraJugada;
 
-let obj1;
-let obj2;
-let contParelles=0;
-
 
 //funcions
 
 function crearCartes(){
-    // let lletresBarrejades = letras.sort((()=>Math.random()-0.5))
+    let lletresBarrejades = letras.sort((()=>Math.random()-0.5))
     for(let i=0;i<letras.length;i++){
         const carta = document.createElement("button")
-        carta.textContent=letras[i]
+        carta.textContent=lletresBarrejades[i]
         carta.id="carta_"+(i+1)
         carta.className="cartes"
         cartes.appendChild(carta)
@@ -31,43 +27,22 @@ crearCartes()
 
 function jugarCarta(obj){
 
+    let lletrajugada1 = obj.id
+
     lletraJugada = obj.textContent
     parell.push(lletraJugada)
     obj.style.color="#000000"
 
-    if(!obj1){
-        obj1 = obj
-    }else{
-        obj2 = obj
-    }
-
     if (parell.length==2) {
-        if(parell[0]==parell[1]){ 
-            obj1=null
-            obj2=null
-            win()
+        if(parell[0]==parell[1]){
+            alert("Parella")    
         }else{
             setTimeout(() => {
-                girarCartes()
-                obj1=null
-                obj2=null
-            }, 1000); 
+                obj.style.color="#00000000"
+            }, 1000);
+    
         } 
         parell=[] 
-    } 
+    }   
 }
-
-function girarCartes(){
-    obj1.style.color="#00000000"
-    obj2.style.color="#00000000"
-}
-
-function win(){
-    contParelles++
-    if (contParelles==10) {
-        window.close()
-    }
-}
-
-//LOCATION.ASSIGN PER REDIRECCIONAR A UNA PESTANYA AL ACABAR EL JOC
 
